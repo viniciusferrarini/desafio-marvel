96441f5667dd27195f8fe7dcde72b3344f5167f2
Vinicius Ferrarini <viniciusferrarini@outlook.com>
2019-06-03 21:36:19 -0300

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,21 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { CharactersComponent } from './characters/characters.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    CharactersComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    CharactersComponent
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
