f04d5b15e3356d021734e59b7747f6013e52fcb2
Vinicius Ferrarini <viniciusferrarini@outlook.com>
2019-06-08 17:08:17 -0300

    adicionar loading

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,5 @@
 export const environment = {
-  production: true
+  production: true,
+  apiUrl: 'https://gateway.marvel.com:443/v1/public/',
+  apiKey: 'ts=1&apikey=8fcd4ed9b297537f2e25e12f658d2596&hash=c2d72c5c69e9c8a0c4a2fcb1ae8e21be'
 };
